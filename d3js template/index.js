4b7e7f4994710ed19a6855a0a14ab3f4c506d065
Shiva Pingle <shivapingle@kubrickgroup.com>
2022-11-13 13:10:54 +0000

    Add d3js basic template for svg creation

---
+++ d3js template/index.js
@@ -0,0 +1,8 @@
+const svgWidth = 700;
+const svgHeight = 500;
+
+const svg = d3.select('body').append('svg')
+    .style('border-style', 'solid')
+    .attr('width', svgWidth)
+    .attr('height', svgHeight);
+
